5b913f400f66a4f53b2969b30468b8f8987c1db2
josephsintum <sjumbam15@gmail.com>
2020-01-15 09:38:45 -0800

    storing only valid forms

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 
 // Link stores information about links
 type Link struct {
-	Title  string
-	URL    string
-	Origin string
+	Title    string
+	URL      string
+	Origin   string
+	FormType string
 }
 
 func main() {
@@ -26,46 +27,72 @@ func main() {
 	links := make(map[string]Link)
 	var currentPage string
 
-	links["0"] = Link{Title: "Title", URL: "URL", Origin: "Origin"}
+	links["0"] = Link{
+		Title:    "Title",
+		URL:      "URL",
+		Origin:   "Origin",
+		FormType: "Form Type",
+	}
 
 	// On every <a> element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		url := e.Attr("href")
 
-		switch true {
+		switch {
 		// check for .pdf
 		case strings.Contains(url, ".pdf"):
-			fmt.Println(".pdf")
+			if !strings.Contains(url, "://") {
+				url = "https://www.sjcc.edu" + url
+			}
+			links["https://www.sjcc.edu"+url] = Link{
+				Title:    e.Text,
+				URL:      url,
+				Origin:   currentPage,
+				FormType: "PDF",
+			}
 
 		// check for Google Forms
 		case (strings.Contains(url, "docs.google.com/forms") || strings.Contains(url, "goo.gl/forms")):
-			fmt.Println("google docs")
+			links["https://www.sjcc.edu"+url] = Link{
+				Title:    e.Text,
+				URL:      url,
+				Origin:   currentPage,
+				FormType: "Google Docs",
+			}
 
 		// check for Office Forms
 		case strings.Contains(url, "forms.office.com/"):
-			fmt.Println("google docs")
+			links["https://www.sjcc.edu"+url] = Link{
+				Title:    e.Text,
+				URL:      url,
+				Origin:   currentPage,
+				FormType: "Office Forms",
+			}
 
 		// check for formsite
 		case strings.Contains(url, "formsite.com"):
-			fmt.Println("formsite")
+			links["https://www.sjcc.edu"+url] = Link{
+				Title:    e.Text,
+				URL:      url,
+				Origin:   currentPage,
+				FormType: "FormSite",
+			}
 
 		// check if link contains form in title
 		case strings.Contains(e.Text, "form"):
-			fmt.Println("form")
-
-		default: 
-			fmt.Println("nothing")
-		}
-
-
-		links[url] = Link{
-			Title:  e.Text,
-			URL:    url,
-			Origin: currentPage,
+			if !strings.Contains(url, "://") {
+				url = "https://www.sjcc.edu" + url
+			}
+			links["https://www.sjcc.edu"+url] = Link{
+				Title:    e.Text,
+				URL:      url,
+				Origin:   currentPage,
+				FormType: "Form",
+			}
 		}
 
 		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, url)
+		// fmt.Printf("Link found: %q -> %s\n", e.Text, url)
 		// Visit link found on page
 		// Only those links are visited which are in Allowed Domains
 		c.Visit(e.Request.AbsoluteURL(url))
@@ -73,17 +100,21 @@ func main() {
 
 	// Before making a request
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		// fmt.Println("Visiting", r.URL.String())
 		currentPage = r.URL.String()
 	})
 
 	// Set error handling
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL: ", r.Request.URL, "failed with response: ", r, "\nError: ", err)
-	})
+	// c.OnError(func(r *colly.Response, err error) {
+	// 	fmt.Println("Request URL: ", r.Request.URL, "failed with response: ", r, "\nError: ", err)
+	// })
 
 	// start scraping
 	c.Visit("https://www.sjcc.edu")
+
+	for _, record := range links {
+		fmt.Println(record)
+	}
 
 	// fmt.Printf("%v \n", links)
 }
