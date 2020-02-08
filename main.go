e7c02bb27ce73b75bde8f70d825ce5ac9fcb9a20
josephsintum <sjumbam15@gmail.com>
2020-02-07 16:39:46 -0800

    adding smartsheet test

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ type Link struct {
 	URL      string
 	Origin   string
 	FormType string
-}
+} 
 
 func main() {
 	c := colly.NewCollector(
 		// Visit only these
-		colly.AllowedDomains("www.sjcc.edu", "sjcc.edu"),
+		colly.AllowedDomains("www.sjcc.edu", "sjcc.edu", "catalog.sjcc.edu"),
 		// Cache responses to prevent multiple download of pages
 		// even if the collector is restarted
 		colly.CacheDir("./sjcc_cache"),
@@ -39,7 +39,7 @@ func main() {
 		url := e.Attr("href")
 
 		switch {
-		// check for .pdf
+		// ? query for .pdf
 		case strings.Contains(url, ".pdf"):
 			if !strings.Contains(url, "://") {
 				url = "https://www.sjcc.edu" + url
@@ -51,7 +51,7 @@ func main() {
 				FormType: "PDF",
 			}
 
-		// check for Google Forms
+		// ? query for Google Forms
 		case (strings.Contains(url, "docs.google.com/forms") || strings.Contains(url, "goo.gl/forms")):
 			links["https://www.sjcc.edu"+url] = Link{
 				Title:    e.Text,
@@ -60,7 +60,7 @@ func main() {
 				FormType: "Google Docs",
 			}
 
-		// check for Office Forms
+		// ? query for Office Forms
 		case strings.Contains(url, "forms.office.com/"):
 			links["https://www.sjcc.edu"+url] = Link{
 				Title:    e.Text,
@@ -69,7 +69,7 @@ func main() {
 				FormType: "Office Forms",
 			}
 
-		// check for formsite
+		// ? query for formsite
 		case strings.Contains(url, "formsite.com"):
 			links["https://www.sjcc.edu"+url] = Link{
 				Title:    e.Text,
@@ -78,17 +78,27 @@ func main() {
 				FormType: "FormSite",
 			}
 
-		// check if link contains form in title or link
-		case strings.Contains(e.Text, "form") || strings.Contains(url, "form"):
-			if !strings.Contains(url, "://") {
-				url = "https://www.sjcc.edu" + url
-			}
+		// TODO add a case for smartsheet forms
+		case strings.Contains(url, "smartsheet.com/b/form"):
 			links["https://www.sjcc.edu"+url] = Link{
 				Title:    e.Text,
 				URL:      url,
 				Origin:   currentPage,
-				FormType: "Form",
+				FormType: "SmartSheets",
 			}
+
+		// check if link contains form in title or link
+		// ! refine query for "form" as word not substring
+		// case strings.Contains(e.Text, "form") || strings.Contains(url, "form"):
+		// 	if !strings.Contains(url, "://") {
+		// 		url = "https://www.sjcc.edu" + url
+		// 	}
+		// 	links["https://www.sjcc.edu"+url] = Link{
+		// 		Title:    e.Text,
+		// 		URL:      url,
+		// 		Origin:   currentPage,
+		// 		FormType: "Form",
+		// 	}
 		}
 
 		// Print link
@@ -104,17 +114,19 @@ func main() {
 		currentPage = r.URL.String()
 	})
 
-	// Set error handling
+	// ! Set error handling
 	// c.OnError(func(r *colly.Response, err error) {
 	// 	fmt.Println("Request URL: ", r.Request.URL, "failed with response: ", r, "\nError: ", err)
 	// })
 
 	// start scraping
-	c.Visit("https://www.sjcc.edu")
+	c.Visit("http://www.sjcc.edu")
 
+	// TODO print as CSV format
+	// TODO remove origin link
 	for _, record := range links {
 		fmt.Println(record)
 	}
-
+	
 	// fmt.Printf("%v \n", links)
 }
